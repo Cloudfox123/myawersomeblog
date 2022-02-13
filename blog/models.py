19ad8260d3e673806cfb37d030215e3f674b3639
cloud <Djusha817@gmail.com>
2022-02-13 14:44:39 +0200

    change in models

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 
-class Blog(models.Model):
+class Post(models.Model):
     blog_title = models.CharField(max_length=150)
-    blog_date = models.DateField( auto_now_add=True)
-    blog_text = models.TextField(300)
+    blog_date = models.DateField()
+    blog_text = models.TextField(max_length=300)
     blog_image = models.ImageField(upload_to='blog_images/')
